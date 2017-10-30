7116f3c1993900b9e6ad0c8c134d29b28b65356f
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-30 15:33:37 -0400

    add validations to user migration

--- db/migrate/20171030190750_create_users.rb
+++ db/migrate/20171030190750_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false, unique: true
       t.string :hashed_password
-      
+
       t.timestamps
     end
   end
