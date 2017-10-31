185447bc403ae788608423f5aac9cbc8e04ff022
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-31 10:19:56 -0400

    add random color attribute to peg initialize method

--- app/models/peg.rb
+++ app/models/peg.rb
@@ -1,4 +1,11 @@
 class Peg < ApplicationRecord
   belongs_to :pegable, polymorphic: true
 
+  COLORS = ["blue", "white", "black", "yellow", "red", "green"]
+
+  def initialize
+    super
+    self.color = COLORS.sample
+  end
+
 end
