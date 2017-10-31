7ef10bebd4e52d8ba1f6133ed12c318dd02e62d1
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-30 23:15:36 -0400

    add guesses_remaining to game migration, default 12

--- db/migrate/20171030190820_create_games.rb
+++ db/migrate/20171030190820_create_games.rb
@@ -4,6 +4,7 @@ class CreateGames < ActiveRecord::Migration[5.1]
       t.integer :user_id, foreign_key: true, null: false
       t.boolean :finished, default: false
       t.boolean :solved, default: false
+      t.integer :guesses_remaining, default: 12
 
       t.timestamps
     end
